b8d77b92bad9e03e032da59f50c356fafa7e7e0c
chihyang <>
2025-09-03 18:34:41 +0800

    Add ChildComponent.vue

---
+++ ChildComponent.vue
@@ -0,0 +1,15 @@
+<template>
+  <div>
+    <input
+      type="text"
+      :value="message"
+      @input="$emit('update:message', $event.target.value)"
+    />
+  </div>
+</template>
+
+<script>
+export default {
+  props: ['message']
+}
+</script>
---
+++ index.vue
@@ -0,0 +1,19 @@
+<template>
+  <div>
+    <child-component :message="text" @update:message="newVal => text = newVal" />
+    <p> MESAGE: {{ text }}</p>
+  </div>
+</template>
+
+<script>
+import ChildComponent from './ChildComponent.vue'
+
+export default {
+  components: { ChildComponent },
+  data() {
+    return {
+      text: 'Hello Vue!'
+    }
+  }
+}
+</script>
